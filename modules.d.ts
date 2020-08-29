6a0b50a253d329e19f66ba8af257e10cfeba3470
Donald Wu <yeukfei02@gmail.com>
2020-08-29 10:34:19 +0800

    feat: 🎸 add expo push notification

--- modules.d.ts
+++ modules.d.ts
@@ -3,3 +3,4 @@ declare module 'react-native-image-slider-box';
 declare module 'react-native-table-component';
 declare module 'react-native-credit-card-input';
 declare module 'react-native-stripe-api';
+declare module '@react-native-community/async-storage/jest/async-storage-mock';
